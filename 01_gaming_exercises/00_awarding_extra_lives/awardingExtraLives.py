070262a3c67bda8cbd5ee7114199805a32c4eee7
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-09-12 13:13:01 -0400

    v0.0
    I worked on code that displays the players amount of score, and lives.

--- 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -2,6 +2,18 @@
 
 
 lives = 3
+score = 25000
+name = "Tegan"
+
+
+
+
+
+# CHARACTER REFEREENCE
+# CURLY BRACES {}
+# BRACKETS []
+# ANGLE-BRACKETS <>
+# PARENTHESES ()
 
 # Allow the user to input the score AS AN INTEGER.
 
@@ -13,4 +25,14 @@ lives = 3
     # Give 2 Extra Lives
 
 # Output the score and number of lives to the screen.
-# ("Output the amount of points scored".\n)
+score = int(input("Please display your score")) # This is where the user types in their score
+
+if score <= 10000:
+    lives -= 1 # Lose a life
+elif score < 1000001: 
+    lives += 1 # Gain a lifes
+elif score > 100000:
+    lives += 2  # Gain 2 lifes
+
+
+print(F"This is your current score {score} This is your current amount of lives {lives}") # This message displays your current score and your current amount of lives
