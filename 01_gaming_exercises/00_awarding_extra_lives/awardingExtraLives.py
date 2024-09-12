d1a07e3d000b97b0fba00d55fd9638f6f31e68c0
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-09-12 11:31:28 -0400

    v0.0
    I worked on awarding my player extra lives.

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -0,0 +1,16 @@
+# Awarding Extra Lives, Tegan Robinson, v0.0
+
+
+lives = 3
+
+# Allow the user to input the score AS AN INTEGER.
+
+# If score is 10000 or less
+    # Lose a life
+# If score is > 10000 but less than 100001
+    # Give 1 Extra Life
+# If score is > 100000
+    # Give 2 Extra Lives
+
+# Output the score and number of lives to the screen.
+# ("Output the amount of points scored".\n)
