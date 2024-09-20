178194585f291f3246783c1b5ee61df4f674d907
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-09-20 13:19:27 -0400

    v0.0
    I worked on my rock paper scissor robot

--- 01_gaming_exercises/04_rock_paper_scissors/rps.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.py
@@ -20,20 +20,56 @@ If you need to write large comments, it's easier to us multi-line strings than p
 
 # MAIN GAME LOOP
 while playerScore < 5 and cpuScore < 5:
-     print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points .\n")
-    playerChoice = input("Please enter rock, paper or siccors and press enter.\n").lower()
-    if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
-        playerChoice = input("Please ebter rock, paper, or scissors and press enter.\n").lower()
-        if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
-        print("You are bit following directions, Please try again.\n")
-        exit()
-    else:
-      print(f"You have chosen {playerChoice}.\n")
+  print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
+  playerChoice = input("Please enter rock, paper or siccors and press enter.\n").lower()
+  if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+     playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+  print("You are bit following directions, Please try again.\n")
+exit() 
+else:
+print(f"You have chosen {playerChoice}.\n")
+else:
+print(f"You have chosen {playerChoice}.\n")
 
 
 
 
-    # let the cpu select choice at random.
-    # compare player choice to cpu choice.
-    # print the results to the screen.
-    # award points to the winner and output results.
+  # let the cpu select choice at random.
+  cpuChoice = random.randint(0,2) # randomly select 0, 1 , or 2.
+  if cupChoice == 0:
+    cpuChoice = "rock"
+  elif cpuChoice == 1:
+  cupChoice = "scissors"
+  elif cpuChoice == 2:
+    cpuChoice = "paper"
+  else:
+    print("Unable to determine CPU choice.\nPlease restart.\n ")
+    exit()
+# print(f"CPU Choice: (cpuChoice)"")
+
+# compare player choice to cpu choice
+if playerChoice == 'rock' and cpuChoice == "paper":
+    print("The CPU choose(cpuChoice) and you choose (playerChoice)")
+    print("The CPU wins a point.\n")
+    cpuScore += 1
+  elif playerChoice ==  "rock" and cpuChoice = "scissors":
+    # PLAYER WINS
+    print(f"The Cpu chose {cpuChoice} and you choose {playerChoice}.\n")
+  print("You win a point.\n")
+    playerScore += 1
+  elif playerChoice == "rock" and cpuChoice == "rock":
+    # DRAW
+    print(f"The Cpu choose {cpuChoice} and you choose {playerChoice}")
+    print("It's a draw!\n.")
+  elif playerChoice == "scissors" and cpuChoice == "rock":
+  # CPU WINS
+    print(f"THE CPU choise {cpuChoice} and you choose {playerchoice}.\n")
+    print('The Cpu wins a point')
+  cpuScore += 1
+  elif playerChoice == "scissors" and the cpuChoice = "paper":
+    # PLAYER WINS
+  print(f"The Cpu chose {cpuChoise} and you choose {playerChoice}.\n")
+  print("You win a point.\n")
+  playerScore += 1 
+  elif playerScore == "scissors" and cpuChoice 
