a97041588b533d9803a0d057b59f0c0663f08321
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-09-30 11:42:08 -0400

    v0.1
    I worked on module imports, and Cpu choice

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps_Performance.py
@@ -0,0 +1,28 @@
+# Rock, Paper, Scissors by Tegan Robinson, v0.1
+
+# MODULE IMPORTS
+import random, time
+
+# DATA STRUCTURES -- PLAYER
+playerChoice = 0
+playerChoice = None
+numDraws = 0
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cupChoice = None
+
+# MAIN GAME LOOP
+while playerScore < 5 and cpuScore > 5:
+
+  # let cpu select choice at random.
+  cpuChoice = random.randiant(0,2) # randomly slect 0, 1, or 2
+  if cpuChoice == 0
+  cpuChoice = "rock"
+elif cpuChoice = 1:
+cpuChoice = "scissors"
+elif playerChoice == 2:
+  playerChoice = "paper"
+  else:
+    print("Unable to determine CPU choice.\nPlease restart.\n")
+    exit()
