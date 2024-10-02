ecdbe4cfce9e591cf38b823f37da07e603edc70a
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-10-02 12:05:02 -0400

    v0.1
    I worked on my rock, paper, and scissors bot

--- 01_gaming_exercises/04_rock_paper_scissors/rps_Performance.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps_Performance.py
@@ -52,4 +52,73 @@ while playerScore < 5 and cpuScore < 5:
   playerChoice = input("Please enter a rock, paper and scissors and press enter.\n"). lower()
   if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n"). lower()
-    if playerChoice
+    if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
+        print("You are not following directions. Please try again.\n")
+        exit()
+        print(f"You have choosen{playerchoice}.\n")
+    else:
+      print(f"You have choosen {playerName}.\n")
+
+# let cpu select choice at random
+cpuChoice = random.randiant(0,2) # randonly slect 0, 1, or 2
+if cpuChoice == 0:
+    cpuChoice = "rock"
+elif cpuChoice == 1:
+   cpuChoice == "scissors"
+elif cpuChoice == 2:
+   cpuChoice = "paper"
+else:
+  print("Unable to determine CPU choice.\nPlease restart.\n")
+  exit()
+# print(f"CPU choice: {cpuChoice}")
+
+# compare player choice to cpu choice
+if playerChoice == "rock" and cpuChoice == "paper"
+  # CPU WINS
+  print(f"The CPU choose {cpuChoice} adn you choose {playerChoice}.\n")
+  print(f"The CPU wins a point")
+  cpuChoice += 1
+elif playerChoice == "rock" and cpuChoice == "scissors":
+  # PLAYER WINS
+  print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+  print(f"You win a point")
+  playerScore += 1
+elif playerChoice == "rock" and cpuChoice == "rock":
+  # DRAW
+   print(f"The CPU chose {cpuChoice} and you choose {playerChoice}.\n")
+   print(f"It's a draw.\n")
+elif playerChoice == "scissors" and cpuChoice == "rock":
+  # CPU WINS
+   print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+   print(f"The CPU wins a point")
+   cpuScore += 1
+elif playerChoice == "scissors" and cpuChoice == "paper":
+# PLAYER WINS
+   print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+   print(f"You win a point.\n")
+   playerScore += 1
+elif playerChoice == "scissors" and cpuChoice == "scissors":
+  # DRAW
+  print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+  print("It's a draw!".\n")
+elif playerChoice == "paper" and cpuChoice == "rock"
+  # PLAYER WINS        
+  print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+  print("You win a point".\n")
+        playerScore += 1
+elif playerChoice == "paper" and cpuChoice == "paper"
+  # DRAW
+  print(f"The CPU choose {cpuChoice} and the player choose {playerChoice}.\n")
+  print("It's a draw".\n")
+elif playerChoice == "paper" and cpuChoice == "scissors":
+  # CPU WINS
+  print(f"The CPU choose {cpuChoice} and you choose {playerChoice}.\n")
+  print("The CPU wins a point.\n")
+      cpuChoice += 1
+  else:
+  print("Unable to determine a winner. Please restart")
+      exit()
+
+
+print(f"Your Final Score: {playerScore}\nCPU Final Score: {cpuScore}\n")
+if playerScore > cpuScore
