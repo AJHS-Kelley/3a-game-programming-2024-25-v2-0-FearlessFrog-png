92047056a0d5a8149a713b171724d6f7c15454f5
FearlessFrog-png <s12118386@students.duvalschools.org>
2024-10-16 13:18:32 -0400

    v3.0
    This is updated rock, paper, and scissors project.

--- 01_gaming_exercises/04_rock_paper_scissors/rps3.0.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rps3.0.py
@@ -1,63 +1,56 @@
-# THE RULES using MULTI_LINE STRINGS
-print("""""S
-Welcome,{Tegan Robinson} to the Rock, Paper, Scissors Robot!      
-It's time to Play Rock, Paper, Scissors!
+# Rock, Paper, Scissors by Ryan Kelley, v3.0
+
+# MODULE IMPORTS
+import random
+
+#DATA STRUCTURES
+player_name = "test player"
+player_score = 0
+player_choice = None
+
       
-You will be going up against the CPU. The first player to score 5 points wins.
-You can select from ROCK, PAPER and SCIRRORS.
-The CPU will select ROCK, PAPER or Scissors at random.
-
-1) PAPER BEATS ROCK
-2) SCISSORS BEAT PAPER
-3) ROCK BEATS SCISSORS                                    
-""")
-
-# MULTI_LINE STRINGS CAN BE USED AS BIG COMMENTS
-"""
-Anything is between the sets of double-quotes is just ignored
-If you need to write large comments, it's easier to us multi-line strings than putting a # in front of 15 different lines
-"""
-
-# MAIN GAME LOOP
-while playerScore < 5 and cpuScore < 5:
-  print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
-  playerChoice = input("Please enter rock, paper or siccors and press enter.\n").lower()
-  if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
-     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
-  playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
-  print("You are not following directions, Please try again.\n")
-  exit() 
+# PlAYER NAME IMPUT
+def playerName() -> str:# Function Signature -- name of function, (arguments if any)
+  """Gets the name form the player and returns it. """
+  # The line is a DOCSTRING, it gives a breif example of what the function does.
+  playerName = input("Please type your name here and press enter.\n")
+  print(f"Hello{playerName}!\n")
+  isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
+if isCorrect == "yes":
+  print(f"okay{player_choice}, let's play Rock, Paper, Scissors")
 else:
-  print(f"You have chosen {playerChoice}.\n")
-  else:
-  print(f"You have chosen {playerChoice}.\n")
+  player = input("Please type your name and press enter.\n")
+  return playerName
 
+ # CALLING THE FUNCTION
+player_name = playerName()
 
+# THE RULES using MULTI-LINE STRINGS
+def rules() -> None:
+ """This function prints the rules for rock, paper, scissors. """  
+print(f"""
+Welcome. {playerName} to the Rock, Paper, Scissors Robot!
+It's time to play Rock, Paper, and Scissors!
 
+You will play against the CPU. The first player to score 5 points wins.
+You will pick from ROCK, PAPER, and Scissors.
+The CPU will select Rock, Paper, and Scissors at random.
 
-  # let the cpu select choice at random.
-  cpuChoice = random.randint(0,2) # randomly select 0, 1 , or 2.
-  if cupChoice == 0:
-    cpuChoice = "rock"
-  elif cpuChoice == 1:
-   cupChoice = "scissors"
-  elif cpuChoice == 2:
+1) ROCK OBLITERATES SCISSORS
+2) SCISSORS SLICES PAPER
+3) PAPER KIDNAPS ROCk
+""")
+# Does another part of this program need to access this information?
+# IF YES, YOU MUST HAVE A return STATEMENT
+# IF NO, A return STATEMENT IS NOT REQUIRED
+
+def playerChoice() -> str:
+ """Allows the CPU to choose rock, paper scissors. """
+ cpuChoice = random.randint(0,2)# randomly select 0, 1, or 2.
+ if cpuChoice == 0:
+   cpuChoice = "rock"
+ elif cpuChoice == 1:
    cpuChoice = "paper"
-  else:
-   print("Unable to determine CPU choice.\nPlease restart.\n ")
-  exit()
-  # print(f"CPU choice: {cpuChoice}")
-
-  # compare player choice to cpu choice
-  if playerChoice == "rock" and cpuChoice == "paper"
-    # CPU WINS
-  print(f"The CPU chose {cpuChoice} and you choose {playerChoice}.\n")
-  print("The CPU wins a point.\n")
-  cpuScore += 1
-  elif playerChoice == "rock" and cpuChoice == "scissors":
-    #PLAYER WINS
-  print(f"The CPU chose {cpuchoice} and you choose {playerChoice}.\n")
-  print("You win a point.\n")
-  playerScore += 1
-  elif playerChoice == "rock" and cpuChoice == "rock"
-    # DRAW
+ elif cpuChoice == 2:
+   cpuChoice = "scissors"
+ elif cpuChoice == 3:
